89a95a432fc8c49c8f61bc619b4cca7e0b621d8d
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2021-08-13 07:49:23 +0300

    05.Basic Theatre Promotion and Foreigh Language

---
+++ JS-Fundamentals/05-Basic-Syntax/foreignLanguage.js
@@ -0,0 +1,18 @@
+function foreignLanguage(lan) {
+    switch (lan) {
+        case "USA":
+        case "England":
+            console.log("English");
+            break;
+        case "Spain":
+        case "Argentina":
+        case "Mexico":
+            console.log("Spanish");
+            break;
+        default:
+            console.log("unknown");
+            break;
+    }
+}
+
+foreignLanguage("Spain")
---
+++ JS-Fundamentals/05-Basic-Syntax/theatrePromotion.js
@@ -0,0 +1,40 @@
+function theatrePromotion(day, age) {
+    let price = 0;
+    switch (day) {
+        case "Weekday":
+            if (age >= 0 && age <= 18) {
+                price = 12;
+            } else if (age > 18 && age <= 64) {
+                price = 18;
+            } else if (age > 64 && age <= 122) {
+                price = 12
+            }
+            break;
+        case "Weekend":
+            if (age >= 0 && age <= 18) {
+                price = 15;
+            } else if (age > 18 && age <= 64) {
+                price = 20;
+            } else if (age > 64 && age <= 122) {
+                price = 15
+            }
+            break;
+        case "Holiday":
+            if (age >= 0 && age <= 18) {
+                price = 5;
+            } else if (age > 18 && age <= 64) {
+                price = 12;
+            } else if (age > 64 && age <= 122) {
+                price = 10;
+            }
+            break;
+
+        default:
+            break;
+    }
+    if (price === 0) {
+        console.log("Error!");
+    } else { console.log(`${price}$`) };
+}
+
+theatrePromotion("Weekday", 42);
